faee0a6a9532a4c1458b72b71ae7a1febf085e89
Piotr Zborowski <piotrz2000zetor@gmail.com>
2024-01-22 00:19:34 +0100

    Radio button group

--- src/main_window.py
+++ src/main_window.py
@@ -11,7 +11,7 @@ class Main_Window(ctk.CTk):
 
         # root settings
         # self.geometry(f'{self.winfo_screenwidth()}x{self.winfo_screenheight()}')
-        self.geometry('1000x700')
+        self.geometry('1000x760')
         self.resizable(False, False)
         self.title('YOLO Object Detector')
         self.iconbitmap('../img/icon.ico')
@@ -21,6 +21,7 @@ class Main_Window(ctk.CTk):
         self.models = get_model_list(PATH_TO_MODELS)
         self.selected_image = None
         self.selected_model = None
+        self.mode = tk.IntVar(master=self, value=1) # 1 - single image, 2 - video
 
         # widgets
         self.image = ctk.CTkImage(light_image=Image.open(f'../img/photo.png'), # place holder image
@@ -40,7 +41,7 @@ class Main_Window(ctk.CTk):
                                       state='readonly',
                                       command=self.image_cmbox_callback,
                                       width=300,
-                                      height=40,
+                                      height=30,
                                       font=ctk.CTkFont(size=15, weight='bold'),
                                       dropdown_font=ctk.CTkFont(size=15, weight='bold'))
         
@@ -48,17 +49,28 @@ class Main_Window(ctk.CTk):
                                       state='readonly',
                                       command=self.model_cmbox_callback,
                                       width=300, 
-                                      height=40,
+                                      height=30,
                                       font=ctk.CTkFont(size=15, weight='bold'),
                                       dropdown_font=ctk.CTkFont(size=15, weight='bold'))
         
         self.image_cmbox_label = ctk.CTkLabel(master=self, text='Select Image:',
-                                   font=ctk.CTkFont(size=20, weight='bold'),
+                                   font=ctk.CTkFont(size=15, weight='bold'),
                                    height=10)
         
         self.model_cmbox_label = ctk.CTkLabel(master=self, text='Select Model:',
-                                   font=ctk.CTkFont(size=20, weight='bold'),
+                                   font=ctk.CTkFont(size=15, weight='bold'),
                                    height=10)
+        
+        self.radiobtn_image = ctk.CTkRadioButton(master=self, text="Single image", 
+                                                 variable=self.mode, value=1,
+                                                 command=self.radiobtn_callback, 
+                                                 font=ctk.CTkFont(size=15, weight='bold'),
+                                                height=10)
+        self.radiobtn_video = ctk.CTkRadioButton(master=self, text="Video", 
+                                                 variable=self.mode, value=2,
+                                                 command=self.radiobtn_callback, 
+                                                 font=ctk.CTkFont(size=15, weight='bold'),
+                                                height=10)
 
         # grid deffinition
         self.rowconfigure(0, weight=10)
@@ -80,6 +92,8 @@ class Main_Window(ctk.CTk):
         self.model_cmbox.grid(row=4, column=0, columnspan=1, padx=10, pady=10, sticky='ew')
         self.image_cmbox_label.grid(row=1, column=0, columnspan=1, padx=20, pady=10, sticky='sw')
         self.model_cmbox_label.grid(row=3, column=0, columnspan=1, padx=20, pady=10, sticky='sw')
+        self.radiobtn_image.grid(row=1, column=1, columnspan=1, padx=10, pady=10, sticky='nsew')
+        self.radiobtn_video.grid(row=2, column=1, columnspan=1, padx=10, pady=10, sticky='nsew')
         
 
     # actions
@@ -104,3 +118,6 @@ class Main_Window(ctk.CTk):
                                     size=(960,540))
         self.image_label.configure(image=self.image)
         self.image_label._image = self.image
+    
+    def radiobtn_callback(self):
+        pass
--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,4 @@
 SUPPORTED_IMAGES_FORMATS = ['png']
 PATH_TO_IMAGES = '../data/img'
 PATH_TO_MODELS = '../models'
+PATH_TO_VIDEOS = '../data/video'
